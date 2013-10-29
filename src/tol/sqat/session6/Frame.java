9fca6bfaf98a0c6c5a92af774548cbc7ac9fd36b
slacko <ancestrall@gmail.com>
2013-10-29 12:19:05 +0200

    first commit

---
+++ src/tol/sqat/session6/Frame.java
@@ -0,0 +1,37 @@
+package tol.sqat.session6;
+
+
+public class Frame {
+
+	//the score of a single frame
+	public int score(){
+		//to be implemented
+		return 0;
+	}
+
+	//returns whether the frame is a strike or not
+	public boolean isStrike(){
+		//to be implemented
+		return false;
+	}
+	
+	//return whether a frame is a spare or not
+	public boolean isSpare(){
+		//to be implemented
+		return false;
+	}
+
+	//return whether this is the last frame of the match
+	public boolean isLastFrame(){
+		//to be implemented
+		return false;
+	}
+
+	//bonus throws
+	public int bonus(){
+		//to be implemented
+		return 0;
+	}
+
+
+}
---
+++ src/tol/sqat/session6/tests/TestBowlingScoreKeeper.java
@@ -0,0 +1,14 @@
+package tol.sqat.session6.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBowlingScoreKeeper {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
